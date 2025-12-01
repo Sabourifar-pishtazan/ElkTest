b875581f0226b8ad0230746cf0b8b4d18b74cd3e
Niloufar sabourifar <149469568+Sabourifar-pishtazan@users.noreply.github.com>
2025-12-01 13:41:11 +0330

    SET

--- Elk/Program.cs
+++ Elk/Program.cs
@@ -39,6 +39,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Host.UseSerilog();
+
+
 builder.Services.AddScoped<ISchaduleService, SchaduleService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- Elk/Service/SchaduleService.cs
+++ Elk/Service/SchaduleService.cs
@@ -20,10 +20,16 @@ namespace Elk.Service
                     {
 
                         throw new InvalidOperationException("Elk Project"+ ":::::This is a test exception for logging::::::");
+                    
                     }
+
                     catch (Exception ex)
                     {
-                        _logger.LogError("Elk Project::::::" + ex.Message?.ToString() + "An exception occurred while executing CreateError method.");
+
+                        ///_logger.LogError("Elk Project::::::" + ex.Message?.ToString() + "An exception occurred while executing CreateError method.");
+                        _logger.LogError(ex, "Elk Project: {Test} - An exception occurred in CreateError method. Iteration: {Iteration}",
+                                        "Elk Project", i);
+
                     }
 
 
@@ -32,14 +38,17 @@ namespace Elk.Service
                 for (int i = 0; i < 100; i++)
                 {
 
-                    _logger.LogInformation("Test ");
+                    //// _logger.LogInformation("Test ");
+                    _logger.LogInformation("Elk Project: Test message. Iteration: {Iteration}", i);
+
 
                 }
-                
+
             }
             catch (Exception ex) {
 
-                _logger.LogError(ex.Message.ToString() + "An exception occurred while executing CreateError method.");
+                _logger.LogError(ex, "Elk Project: {Test} - An unexpected exception occurred in CreateError method",
+                                     "Elk Project");
             }
         }
     }
